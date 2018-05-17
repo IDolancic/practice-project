3ec0ff11c1d256d9d01b4f6a5bcf180612e13f0a
Ivan Dolančić <idolancic@gmail.com>
2018-05-17 13:33:19 +0200

    1.zadatak (If i variijable) gotov

--- src/hello/HelloWorld.java
+++ src/hello/HelloWorld.java
@@ -2,6 +2,22 @@ package hello;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello, World test");
+		System.out.println("Pocetak programa");
+		
+		int a = 23;
+		/*
+		 * Ako je broj a veci od 15 a manji od 30 ispisi na ekran
+		 */
+		if (a > 15) {
+			System.out.println("Usao u if");
+			System.out.println(a);
+			
+			
+			System.out.println("Kraj ifa");
+		}
+		
+		System.out.println("Kraj programa");
+		
 	}
 }
+ 
