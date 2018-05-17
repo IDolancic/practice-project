452d99b3895dc06bda0964b411a4ebf52b575215
Ivan Dolančić <idolancic@gmail.com>
2018-05-17 14:45:37 +0200

    Dodao zadatak(riješeno)

--- src/hello/HelloWorld.java
+++ src/hello/HelloWorld.java
@@ -2,22 +2,12 @@ package hello;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Pocetak programa");
-		
-		int a = 23;
-		/*
-		 * Ako je broj a veci od 15 a manji od 30 ispisi na ekran
-		 */
-		if (a > 15) {
-			System.out.println("Usao u if");
-			System.out.println(a);
-			
-			
-			System.out.println("Kraj ifa");
+	
+		double b = 0.1;
+		while (b < 1.0) {
+			b =b + 0.1;
+			System.out.println(b);
 		}
-		
-		System.out.println("Kraj programa");
 		
 	}
 }
- 
